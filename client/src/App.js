5947cf26afae4268169a2cfab9ca5981662dc518
Brandon Drury <brandondru00@gmail.com>
2023-02-26 23:45:55 -0500

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import Logo from "./Logo";
+
 const App = () => {
-  return <h1>git pull test meow</h1>;
+  return (
+    <>
+      <h1>git pull test meow</h1>
+      <Logo />
+    </>
+  );
 };
 
 export default App;
